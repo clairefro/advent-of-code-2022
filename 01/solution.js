73c2a30c870aee0bae0ac3a69ebe5b1d31033423
clairefro <claire.froelich@gmail.com>
2023-01-15 14:07:23 -0500

    Add day 1

--- 01/solution.js
+++ 01/solution.js
@@ -3,4 +3,22 @@ const load = require("../util/load");
 
 const raw = load(__dirname + "/input.txt");
 
-console.log({ raw });
+/** ------------------------------------- */
+
+const groups = raw
+  .split("\n\n")
+  .map((i) => i.split("\n").map((n) => parseInt(n)));
+
+const sum = (arr) => arr.reduce((a, b) => a + b);
+
+const sums = groups.map(sum);
+
+const sortedDesc = sums.sort((a, b) => b - a);
+
+/** Highest calorie */
+console.log("## PART 1 ##");
+console.log(sortedDesc[0]);
+
+/** Top 3 elves calories sum */
+console.log("## PART 2 ##");
+console.log(sortedDesc.slice(0, 3).reduce((a, b) => a + b));
